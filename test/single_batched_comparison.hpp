b5ce4bc7b9bce021ea74e0e6ae6da96ecffa2994
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-04-16 16:52:43 -0400

    test(cuSOLVER): continued work on benchmark

--- test/single_batched_comparison.hpp
+++ test/single_batched_comparison.hpp
@@ -2,6 +2,13 @@
 #include <fstream>
 #include <string>
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+#include "cuadmm/cusolver.h"
+
+#define SINGLE_BATCHED_COMP_RESTARTS 1
 #define SINGLE_BATCHED_COMP_MAT_SIZES {2, 3, 4}
 #define SINGLE_BATCHED_COMP_MAT_NUMS {1, 2, 3}
 #define EIG_STREAM_NUM_PER_GPU 15
@@ -10,10 +17,16 @@ float test_single_performance(
     DeviceSolverDnHandle& handle,
     SingleEigParameter& param,
     const int mat_size,
-    const int mat_num
+    const int mat_num,
+    std::vector<double> mat_vals
 ) {
     // compute the buffer size
     // (we do not time this part since it is done only once)
+
+    // create the threads
+    std::condition_variable main_cv;
+    std::mutex main_mtx;     // main thread mutex
+    std::mutex resource_mtx; // ressource mutex
 
     return std::sqrt(2);
 }
@@ -22,9 +35,46 @@ float test_batched_performance(
     DeviceSolverDnHandle& handle,
     BatchEigParameter& param,
     const int mat_size,
-    const int mat_num
+    const int mat_num,
+    std::vector<double> mat_vals
 ) {
-    return std::sqrt(3);
+    // compute the buffer size
+    // (we do not time this part since it is done only once)
+    DeviceDenseVector<double> mat(GPU0, mat_size * mat_size * mat_num);
+    DeviceDenseVector<double> W(GPU0, mat_size * mat_num); // eigenvalues
+    DeviceDenseVector<int> info(GPU0, mat_num);
+    int buffer_size = batch_eig_get_buffersize_cusolver(
+        handle, param, mat, W,
+        mat_size, mat_num
+    );
+    DeviceDenseVector<double> buffer(GPU0, buffer_size);
+
+    // copy matrices from CPU to 
+    CHECK_CUDA ( cudaMemcpy(mat.vals, mat_vals.data(), mat_size * mat_size * mat_num * sizeof(double), cudaMemcpyHostToDevice) );
+
+    // timing
+    cudaEvent_t start, stop;
+    float milliseconds = 0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // compute the eigenvalues and eigenvectors
+    cudaEventRecord(start);
+    batch_eig_cusolver(
+        handle, param,
+        mat, W,
+        buffer, info,
+        mat_size, mat_num, buffer_size
+    );
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    // end
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    return milliseconds;
 }
 
 TEST(SingleBatchedComparison, Default)
@@ -57,29 +107,37 @@ TEST(SingleBatchedComparison, Default)
     std::vector<int> mat_sizes = SINGLE_BATCHED_COMP_MAT_SIZES;
     std::vector<int> mat_nums = SINGLE_BATCHED_COMP_MAT_NUMS;
 
-    DeviceSolverDnHandle handle;
-    handle.set_gpu_id(GPU0);
+    DeviceSolverDnHandle handle(GPU0);
     handle.activate();
     SingleEigParameter single_param(GPU0);
     BatchEigParameter batch_param(GPU0);
 
-    for (auto mat_size : mat_sizes) {
-        for (auto mat_num : mat_nums) {
-            /* Test the single QR performance */
-            float single = test_single_performance(
-                handle, single_param, mat_size, mat_num
-            );
+    // TODO: warm-up the GPU
 
-            /* Test the batched Jacobi performance */
-            float batched = test_batched_performance(
-                handle, batch_param, mat_size, mat_num
-            );
-
-            output_file << mat_size << " " << mat_num << " "
-                        << single << " " << batched << "\n";
-            
-            std::cout << "mat_size: " << mat_size << ", mat_num: " << mat_num
-                      << ", single: " << single << ", batched: " << batched << "\n";
+    for (int r = 0; r < SINGLE_BATCHED_COMP_RESTARTS; r++) {
+        for (auto mat_size : mat_sizes) {
+            for (auto mat_num : mat_nums) {
+                // TODO: generate random matrices
+                std::vector<double> mat_vals = std::vector<double>(mat_size * mat_size * mat_num, 0.0);
+    
+                // test the single QR performance
+                float single = test_single_performance(
+                    handle, single_param, mat_size, mat_num, mat_vals
+                );
+    
+                // test the batched Jacobi performance
+                float batched = test_batched_performance(
+                    handle, batch_param, mat_size, mat_num, mat_vals
+                );
+    
+                // write the results to the file
+                output_file << mat_size << " " << mat_num << " "
+                            << single << " " << batched << "\n";
+                
+                std::cout << "mat_size: " << mat_size << ", mat_num: " << mat_num
+                          << ", single: " << single << ", batched: " << batched << "\n";
+            }
         }
     }
+    
 }
