88272d03784f4725b095ca2d631b76b6a1b37029
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-04-17 11:41:03 -0400

    feat(plots): added comparison single/batched

--- test/single_batched_comparison.hpp
+++ test/single_batched_comparison.hpp
@@ -10,7 +10,7 @@
 #include "cuadmm/cusolver.h"
 
 #define SINGLE_BATCHED_COMP_RESTARTS 1
-#define SINGLE_BATCHED_COMP_MAT_SIZES {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20, 21}
+#define SINGLE_BATCHED_COMP_MAT_SIZES {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41}
 #define SINGLE_BATCHED_COMP_MAT_NUMS {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 #define EIG_STREAM_NUM_PER_GPU 15
 
