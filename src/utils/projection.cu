be6870dfd89f9730dc6fdf78504d56dc023f827a
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-06-02 14:32:47 -0400

    feat(projection): add Frobenius norm scaling

--- src/utils/projection.cu
+++ src/utils/projection.cu
@@ -132,6 +132,14 @@ void projection_TF16(
     CHECK_CUDA( cudaMemcpy(dA_orig, A_h.data(), nn * sizeof(float), H2D) );
     CHECK_CUDA( cudaMemcpy(dA_our,  A_h.data(), nn * sizeof(float), H2D) );
 
+    float frob_norm;
+    CHECK_CUBLAS( cublasSnrm2(cublasH, nn, dA_orig, 1, &frob_norm) );
+
+    // scale to have eigenvalues in [-1, 1]
+    const float scale = frob_norm > 0.0f ? frob_norm : 1.0f;
+    const float inv_scale = 1.0f/scale;
+    CHECK_CUBLAS( cublasSscal(cublasH, nn, &inv_scale, dA_our, 1) );
+
     // build identity I on device
     std::vector<float> I_h(nn, 0.0f);
     for (int i = 0; i < n; i++) I_h[i*n + i] = 1.0f;
@@ -330,6 +338,7 @@ void projection_TF16(
 
     CHECK_CUDA( cudaMemcpy(dA_our, dTmp, nn*sizeof(float), cudaMemcpyDeviceToDevice) );
     CHECK_CUBLAS( cublasSscal(cublasH, nn, &half, dA_our, 1) );
+    CHECK_CUBLAS( cublasSscal(cublasH, nn, &scale, dA_our, 1) );
     CHECK_CUDA( cudaDeviceSynchronize() );
 
     // copy the result back to mat by converting the float matrix to double using the host
