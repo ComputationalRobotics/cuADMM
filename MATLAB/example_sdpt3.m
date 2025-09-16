a5dc362ccbe9f7aacccd52634c4388bff5f9e314
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-09-16 06:27:38 -0400

    minor changes

--- MATLAB/example_sdpt3.m
+++ MATLAB/example_sdpt3.m
@@ -5,7 +5,7 @@ addpath("./build");
 addpath("../examples/utils");
 addpath("../examples/mexfiles")
 
-sdpt3 = load("/home/jordan/antoine/sdp_problems/sdpt3/e_moment_stable_17_0.5_2_2.mat");
+sdpt3 = load("/home/jordan/antoine/sdp_problems/sdpt3/vibra4.mat");
 
 %% Convert SDPT3 to cuADMM format
 [At, b, C, blk_sizes, blk_vals] = sdpt3_to_cuadmm(sdpt3);
@@ -17,16 +17,29 @@ con_num = size(At, 2);
 X_new = zeros(vec_len, 1);
 y_new = zeros(con_num, 1);
 S_new = zeros(vec_len, 1);
-sig_new = 1.0;
+sigma = 1.0;
 
 eig_stream_num_per_gpu = 12;
-max_iter = 1e8;
+max_iter = 50;
 stop_tol = 1e-4;
+
+addpath(genpath("/home/jordan/antoine/admmSDP"));
+addpath(genpath("~/matlab-install/SDPT3-4.0"));
+optionsADMM.maxIter = max_iter;
+optionsADMM.stopTol = stop_tol;
+optionsADMM.printyes = 1;
+optionsADMM.scaleA = 1;
+optionsADMM.scaleData = 1;
+optionsADMM.sig = sigma;
+optionsADMM.partialProj = 0;
+optionsADMM.useLowRankEigs = 0;
+optionsADMM.epsy = 1e-16;
+[X, y, S, infoADMM] = ADMM(sdpt3.blk, sdpt3.At, sdpt3.b, sdpt3.C, optionsADMM); return;
 
 [X_out, y_out, S_out, sig_out] = cuadmm_MATLAB(eig_stream_num_per_gpu,...
                                                 max_iter, stop_tol,...
                                                 At, b, C, blk_sizes, blk_vals,...
-                                                X_new, y_new, S_new, sig_new);
+                                                X_new, y_new, S_new, sigma);
 
 function [cuda_At, cuda_b, cuda_C, cuda_blk_sizes, cuda_blk_vals] = sdpt3_to_cuadmm(sdpt3)
     if iscell(sdpt3.At)
